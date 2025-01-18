c7a2f60e11a0c35bd5c27d5eac378a7b15b802ef
Dmitry Sinyagin <itk_system@mail.ru>
2025-01-18 22:00:06 +0300

    add message object

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,11 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const userHelper = require('openfsm-user-helper');
-const REGISTRATION_SUCCESS_MSG   = 'Пользователь зарегистрирован успешно';
-const USER_LOGOUT_MSG   = 'Вы вышли из системы.';
-const SERVER_ERROR_MSG = 'Server error';
 const tokenExpiredTime = '3h'; // Время жизни токена
 const pool = require('openfsm-database-connection-producer');
 const common = require('openfsm-common');  /* Библиотека с общими параметрами */
+const MESSAGES = require('openfsm-common-auth-services').MESSAGES;  /* Библиотека с общими параметрами для Auth*/
+const LANGUAGE = 'RU';
 const CommonFunctionHelper = require("openfsm-common-functions")
 const _response= new CommonFunctionHelper();
 const cookieParser = require('cookie-parser');
@@ -28,27 +27,26 @@ const { DateTime } = require('luxon');
 
 exports.register = async (req, res) => {
   try {
-    console.log('Received data:', req.body);
     const { email, password } = req.body;
-    if (!email || !password) throw new AuthError(400, 'Email и пароль обязательны.'); 
+    if (!email || !password) throw new AuthError(400, MESSAGES[LANGUAGE].EMAIL_AND_PASSWORD_REQUIRED ); 
 
     const existingUser = await userHelper.findByEmail(email); // Проверяем наличие пользователя в БД
-    if (existingUser) throw new AuthError(409, 'Такой пользователь уже существует.'); 
+    if (existingUser) throw new AuthError(409, MESSAGES[LANGUAGE].USER_ALREADY_EXISTS); 
 
     const hashedPassword = await bcrypt.hash(password, 10); // Хэшируем пароль
-    if (!hashedPassword) throw new AuthError(500, 'Ошибка при хэшировании пароля.');    
+    if (!hashedPassword) throw new AuthError(500, MESSAGES[LANGUAGE].PASSWORD_HASHING_ERROR);    
 
     const userId = await userHelper.create(email, hashedPassword); // Создаем нового пользователя
-    if (!userId) throw new AuthError(500, 'Ошибка при создании пользователя.');    
+    if (!userId) throw new AuthError(500, MESSAGES[LANGUAGE].USER_CREATION_ERROR);    
     
     const user = await userHelper.findById(userId); // Получаем данные нового пользователя
-    if (!user)  throw new AuthError(500, 'Ошибка при создании пользователя.');    
+    if (!user)  throw new AuthError(500, MESSAGES[LANGUAGE].USER_CREATION_ERROR);    
 
     await userHelper.setCustomerRole(user.getId(), common.USER_ROLES.CUSTOMER);    // Устанавливаем роль "Клиент"
     await userHelper.sendMessage(userHelper.PAYMENT_ACCOUNT_CREATE_QUEUE, { userId: user.getId() }); // Отправляем сообщение для создания счета    
     await userHelper.sendMessage(userHelper.MAIL_QUEUE, userHelper.getRegistrationMail(user)); // Отправляем приветственное письмо
     
-    res.status(201).json({ message: REGISTRATION_SUCCESS_MSG }); // Успешная регистрация
+    res.status(201).json({ message: MESSAGES[LANGUAGE].USER_REGISTERED_SUCCESSFULLY}); // Успешная регистрация
   } catch (error) {       
     _response.sendError(req, res, error); 
   }
@@ -59,10 +57,10 @@ exports.register = async (req, res) => {
 exports.login = async (req, res) => {
   try {
    const { email, password } = req.body;
-   if (!email || !password)  throw new AuthError(400, 'Email и пароль обязательны.');   
+   if (!email || !password)  throw new AuthError(400, MESSAGES[LANGUAGE].EMAIL_AND_PASSWORD_REQUIRED);   
 
     const user = await userHelper.findByEmail(email);  // находим пользователя в БД
-    if (!user) throw new AuthError(422,  "Пользователь не зарегистрирован"); 
+    if (!user) throw new AuthError(422, MESSAGES[LANGUAGE].USER_NOT_FOUND); 
    
     const isMatch = await bcrypt.compare(password, user.getPassword()); // сравниваем хэш пароля, вынесли в отдельную функцию чтобы sql-inject снизить
     if (!isMatch) throw new AuthError(403,  commonFunction.getDescriptionByCode(403)); 
@@ -99,11 +97,11 @@ exports.health = async (req, res) => {
 exports.getPermissions = async (req, res) => {
   try {
     const userId = req.user?.id;    
-    if (!userId) throw new AuthError(400,  'Идентификатор пользователя отсутствует.' );  // Проверка наличия userId
+    if (!userId) throw new AuthError(400, MESSAGES[LANGUAGE].USER_ID_MISSING);  // Проверка наличия userId
     
     const userPermissions = await userHelper.getPermissions(userId);   // Получаем права пользователя
     if (!userPermissions || userPermissions.permissions.length === 0)  // Проверка наличия прав
-      throw new AuthError(403,  'Доступ запрещен: права пользователя отсутствуют.' );      
+      throw new AuthError(403, MESSAGES[LANGUAGE].USER_RIGHTS_MISSING);      
 
      return res.status(200).json({ userPermissions });
    } catch (error) {
@@ -115,10 +113,10 @@ exports.getPermissions = async (req, res) => {
 exports.getMe = async (req, res) => {
   try {
     const userId = req.user?.id;   
-    if (!userId)  throw new AuthError(400,  'Идентификатор пользователя отсутствует.' );  // Проверка наличия userId
+    if (!userId)  throw new AuthError(400,  MESSAGES[LANGUAGE].USER_ID_MISSING );  // Проверка наличия userId
     
     const login = await userHelper.getMe(userId); // Получаем данные пользователя    
-    if (!login) throw new AuthError(402, 'Пользователь не найден.'  );  // Проверка наличия данных пользователя
+    if (!login) throw new AuthError(402, MESSAGES[LANGUAGE].USER_NOT_FOUND  );  // Проверка наличия данных пользователя
     
     return res.status(200).json(login); // Успешный ответ
 
@@ -132,23 +130,14 @@ exports.getToken = (req, res) => {
   try {
     // Получение токена из заголовков авторизации
     const authHeader = req.headers['authorization'];
-    const tokenFromHeader = authHeader?.split(' ')[1] || null;
-
-    // Получение токена из cookies
-    const tokenFromCookies = req.cookies?.accessToken || null;
-
-    // Возврат токена из заголовка или cookies, если доступно
-    const token = tokenFromHeader || tokenFromCookies;
-
-    if (!token) {
-      // Если токен отсутствует, возвращаем null
-      return null;
-    }
-
+    const tokenFromHeader = authHeader?.split(' ')[1] || null;    
+    const tokenFromCookies = req.cookies?.accessToken || null; // Получение токена из cookies    
+    const token = tokenFromHeader || tokenFromCookies; // Возврат токена из заголовка или cookies, если доступно
+    // Если токен отсутствует, возвращаем null
+    if (!token) return null;
     return token;
-  } catch (error) {
-    // Логирование ошибки, если что-то пошло не так
-    logger.error('Ошибка при извлечении токена:', error);
+  } catch (error) {    
+    logger.error(MESSAGES[LANGUAGE].TOKEN_EXTRACTION_ERROR, error); // Логирование ошибки, если что-то пошло не так
     return null;
   }
 };
@@ -173,7 +162,7 @@ exports.checkToken = async (req, res) => {
 
   } catch (error) {
     // Добавляем токен в черный список, если ошибка связана с авторизацией
-    if ( (error instanceof CustomError) && rror?.code === 401 && token) {
+    if ( (error instanceof CustomError) && error?.code === 401 && token) {
       exports.tokenBlacklist.add(token);
     }
     _response.sendError(req, res, err);     
@@ -182,17 +171,15 @@ exports.checkToken = async (req, res) => {
 
 
 exports.logout = async (req, res) => {
-  try {
-    // Получаем токен
-    const token = exports.getToken(req, res);
+  try {  
+    const token = exports.getToken(req, res);  // Получаем токен
     if (!token) throw new AuthError(401,  commonFunction.getDescriptionByCode(Number(error) || 500 ));  
     
     if (exports.tokenBlacklist.has(token)) // Проверяем, находится ли токен в черном списке
         throw new AuthError(401,  commonFunction.getDescriptionByCode(Number(error) || 500 ));  
 
-    exports.tokenBlacklist.add(token);    // Добавляем токен в черный список    
-
-    return res.status(200).json({ message: USER_LOGOUT_MSG }); // Возвращаем успешный ответ
+     exports.tokenBlacklist.add(token);    // Добавляем токен в черный список    
+     return res.status(200).json({ message: MESSAGES[LANGUAGE].USER_LOGOUT }); // Возвращаем успешный ответ
   } catch (error) {    
     _response.sendError(req, res, err); // Обработка ошибок
   }
@@ -213,12 +200,10 @@ exports.tokenVerification = async (req, res) => {
     });
     // Сохраняем данные пользователя и токен в объект запроса
     req.user = user;
-    req.token = token;
-    // Возвращаем успешный результат
-    return true;
-  } catch (error) {
-    // Логируем ошибку для отладки
-    console.error(`Token verification failed: ${error}`);
+    req.token = token;  
+    return true;   // Возвращаем успешный результат
+  } catch (error) {    
+    console.error(MESSAGES[LANGUAGE].TOKEN_VERIFICATION_FAILED,` ${error}`); // Логируем ошибку для отладки
     return false; // Возвращаем false в случае ошибки
   }
 };
@@ -230,8 +215,8 @@ exports.checkVerificationCode = async (req, res) => {
     const { verificationCode } = req.body;
     const user = await userHelper.findById(userId);
     console.log(userId, verificationCode, user.getVerificationCode());
-    if (!user) throw new AuthError(409,  'Пользователь не найден' );  
-    if (!user.getVerificationCode()) throw new AuthError(400,  'Ошибка получения кода верификации' );  
+    if (!user) throw new AuthError(409, MESSAGES[LANGUAGE].USER_NOT_FOUND );  
+    if (!user.getVerificationCode()) throw new AuthError(400, MESSAGES[LANGUAGE].VERIFICATION_CODE_RETRIEVAL_FAILED);  
 
     let storageCode = exports.verificationCodeStorage.get(user.getId()) || null;    
     if (!user.getConfirmed()) { // Если пользователь еще не подтвержден
@@ -244,24 +229,24 @@ exports.checkVerificationCode = async (req, res) => {
            return res.status(401).json({
            status: false,
            retry: exports.verificationCodeStorage.maxRetry,
-           message: 'Количество попыток исчерпано',
+           message: MESSAGES[LANGUAGE].ATTEMPTS_EXHAUSTED,
         });
       }
       // Проверяем код
       console.log(Number(verificationCode) === Number(storageCode.code),verificationCode, storageCode.code)
       if (Number(verificationCode) === Number(storageCode.code)) {
         const confirmResult = await userHelper.setConfirmed(user.getId());
-        if (!confirmResult) throw new AuthError(500,  'Server error' );  
+        if (!confirmResult) throw new AuthError(500,  MESSAGES[LANGUAGE].OPERATION_FAILED );  
         // Подтверждение успешно
           exports.verificationCodeStorage.delete(user.getId());
-          return res.status(200).json({ status: true, message: 'Регистрация подтверждена' });
+          return res.status(200).json({ status: true, message: MESSAGES[LANGUAGE].REGISTRATION_CONFIRMED });
          } else { // Код неверен
         exports.verificationCodeStorage.incrementRetry(user.getId())
-        return res.status(422).json({status: false, retry: storageCode.retry,message: 'Код неверен',});
+        return res.status(422).json({status: false, retry: storageCode.retry,message: MESSAGES[LANGUAGE].INVALID_CODE});
       }
     }
     // Пользователь уже подтвержден
-    res.status(200).json({ status: true, message: 'Регистрация уже подтверждена' });
+    res.status(200).json({ status: true, message: MESSAGES[LANGUAGE].REGISTRATION_ALREADY_CONFIRMED });
   } catch (error) {    
     _response.sendError(req, res, err); // Обработка ошибок
   }
@@ -277,13 +262,13 @@ exports.resendVerificationCode = async (req, res) => {
     const setCodeResult = await userHelper.changeVerificationCode(userId, newVerificationCode);
     const unConfirmResult = await userHelper.setUnConfirmed(userId);
 
-    if (!setCodeResult || !unConfirmResult) throw new AuthError(500,  'Server error' );  
+    if (!setCodeResult || !unConfirmResult) throw new AuthError(500, MESSAGES[LANGUAGE].OPERATION_FAILED);  
     
     const user = await userHelper.findById(userId); // Получение информации о пользователе
     if (!user) throw new AuthError(404,  commonFunction.getDescriptionByCode(Number(error) || 500 ));  
     
     await userHelper.sendMessage(userHelper.MAIL_QUEUE, userHelper.getNewVerificationCodeMail(user)); // Отправка письма с новым кодом    
-    res.status(200).json({ status: true, message: 'Код сменен. Введите новый код.' }); // Успешный ответ
+    res.status(200).json({ status: true, message: MESSAGES[LANGUAGE].CODE_CHANGED_ENTER_NEW }); // Успешный ответ
 
   } catch (error) {
     _response.sendError(req, res, err); // Обработка ошибок
